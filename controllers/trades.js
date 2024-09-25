2663c93f9956b44316694fac5a3be53d9734d8a9
KeneHermitCoder <kene.nnakwue@notjustevent.com>
2024-09-25 06:54:56 +0000

    initial commit

---
+++ controllers/trades.js
@@ -0,0 +1,47 @@
+const Trade = require('../models/trades');
+
+module.exports = {
+    create: async function (req, res) {
+        const newTrade = new Trade(req.body);
+        await newTrade.save();
+        return res.status(201).json(newTrade);
+    },
+
+    getAll: async function (req, res) {
+        try {
+            const foundTrades = await Trade.findAll();
+            res.status(200).json(foundTrades);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    },
+
+    getById: async function (req, res) {
+        try {
+            const foundTrade = await Trade.findByPk(req.params.id);
+            if (!foundTrade) return res.status(404).send('ID not found');
+            res.status(200).json(foundTrade);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        };
+    },
+
+    partialUpdate: async function (req, res) {
+        res.status(405).send();
+    },
+
+    update: async function (req, res) {
+        try {
+            const updatedTrade = await Trade.update(req.body, {
+                where: { id: req.params.id }
+            });
+            return res.status(405).json({ message: 'Trade updates not allowed at this time' });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        };
+    },
+
+    delete: async function (req, res) {
+        res.status(405).send();
+    },
+};
